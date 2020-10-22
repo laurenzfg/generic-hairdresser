3395b6375105d8779f14aa95958c8ca99c486238
Laurenz Grote <laurenzfg@laurenzgrote.de>
2020-10-22 17:53:14 +0200

    Simple Navigation

--- src/components/mainmenu.js
+++ src/components/mainmenu.js
@@ -1,33 +1,43 @@
 import React from "react"
+import { StaticQuery, graphql, Link } from "gatsby"
 import logo from "../images/logo.png"
 
-export default function MainMenu() {
+export default function MainMenu({ data }) {
 	return (
-	  <div class="flex flex-col space-y-2 text-gray-900">
+    <StaticQuery
+    query={graphql`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                path
+                title
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div class="flex flex-col space-y-2 text-gray-900">
       <img
         src={logo}
         alt="Logo"
         class="px-4 py-2 w-full h-16 lg:h-20 object-left object-contain"
       />
-      {/* for every menu item; active highlighting */}
-      <div
-        class="rounded transition-colors ease-in-out duration-500"
-      >
+      {/* active highlighting */}
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div class="rounded">
         <div
           class="px-4 py-2 break-words md:hover:text-cd-red transition-colors ease-in-out duration-200"
         >
-          Damen
+          <p><Link to={node.frontmatter.path}>{node.frontmatter.title}</Link></p>
         </div>
       </div>
-      <div
-        class="rounded transition-colors ease-in-out duration-500"
-      >
-        <div
-          class="px-4 py-2 break-words md:hover:text-cd-red transition-colors ease-in-out duration-200"
-        >
-          Herren
-        </div>
-      </div>
+      ))}
 	  </div>
+    )}
+    />
 	)
 }
